8e252f0fe4537ba5a4227465260d8f3bd972bb95
sahilpalme <sahilpal.me@gmail.com>
2026-09-05 01:26:24 +0530

    Added the totalexpense fxn and improve addexpense

--- main.py
+++ main.py
@@ -4,28 +4,29 @@ import pandas as pd
 def add_expense():
     main_list = []
     a = ''
-    while a != 'q':
+    while True:
         a = input("Enter the reason(Enter q to quit): ")
         if a == 'q':
             break
-        b = int(input("Enter the amount spent: "))
-        c = (input("Enter the date: "))
+        else:
+            b = int(input("Enter the amount spent: "))
+            c = (input("Enter the date: "))
+            main_list.append({'Date':c,'Reason':a,'Amount':b})
 
-        main_list.append({'Date':c,'Reason':a,'Amount':b})
-
-        df = pd.DataFrame(main_list)
-        print(df)
         #pandas part
-        filename= 'user_database.xlsx'
-        df_existing = pd.read_excel(filename)
-        newdata = pd.DataFrame(main_list)
-        df_updated = pd.concat([df_existing, newdata], ignore_index=True)
-        df_updated.to_excel(filename, index=False)
-        t = 0
-        for i in range(7):
-            z = df_updated.loc[i,'Amount']
-            t = t+z
-        print(f'Total expense: {t}')
-        print("\nData captured successfully in DataFrame:")
+    filename= 'user_database.xlsx'
+    df_existing = pd.read_excel(filename)
+    newdata = pd.DataFrame(main_list)
+    df_updated = pd.concat([df_existing, newdata], ignore_index=True)
+    df_updated.to_excel(filename, index=False)
+    print("\nData captured successfully in DataFrame:")
+
+
+def total_expense():
+    df = pd.read_excel('user_database.xlsx')
+    t = df['Amount'].sum()
+    print(f'Total expense: {t}')
+
 
 add_expense()
+total_expense()
